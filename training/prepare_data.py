f9aedf9dfd914cd7633d925aeeaa261e9cc680da
c-rti <isntsanchez@gmail.com>
2025-12-26 21:18:16 -0500

    update

--- training/prepare_data.py
+++ training/prepare_data.py
@@ -6,6 +6,7 @@ from src.execution.storage import Storage
 
 '''
 this is only to be used for the first run
+from here on out, training data will be pulled through the s3 bucket
 '''
 
 # storage = Storage(s3_bucket=S3_BUCKET)
@@ -13,7 +14,9 @@ this is only to be used for the first run
 # with open("training/data/intents.json", "r") as f:
 #     data = json.load(f)
 
-# data_str = json.dumps(data)
-# data_bytes = data_str.encode('utf-8')
+# df = pd.DataFrame(data[""], columns=["query", "intent"])
+# buffer = io.BytesIO()
+# test = df.to_csv(buffer, index=False)
+# buffer.seek(0)
 
-# storage.putObject(data=data_bytes, s3_key='datasets/intent_classifier/v000/train.json')
+# storage.putObject(data=buffer, s3_key='datasets/intent_classifier/v000/train.json')
